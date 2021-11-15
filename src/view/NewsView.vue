a3137238db7ae0f12ca3f5dc3f3f4b3f7a463e70
office1201 <office.div1201@gmail.com>
2021-11-15 19:53:39 +0900

    Add: axios 설치 및 테스트 api 호출

--- src/view/NewsView.vue
+++ src/view/NewsView.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>news</div>
+  <div>
+    <div v-for="user in users" v-bind:key="user.id">{{ user.title }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      users: [],
+    };
+  },
+  created: function () {
+    // new Promise() 기반으로서 다음을 반환
+    let vm = this;
+    axios
+      .get("https://api.hnpwa.com/v0/news/1.json")
+      .then(function (response) {
+        vm.users = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+};
 </script>
 
 <style>

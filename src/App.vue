e0f451a936909e46cdde95cc7105cb2b7cb59181
office1201 <office.div1201@gmail.com>
2021-11-15 18:42:36 +0900

    Add: redirect 속성 추가 및 router-lint 표현

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
+    <ToolBar></ToolBar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {};
+import ToolBar from "./components/ToolBar.vue";
+
+export default {
+  components: {
+    ToolBar,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/ToolBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <router-link to="/news">news</router-link>
+    <router-link to="/ask">ask</router-link>
+    <router-link to="/jobs">jobs</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

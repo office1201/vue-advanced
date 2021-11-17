8376a95d51a647558e2196da219fcd29b952ac0d
office1201 <office.div1201@gmail.com>
2021-11-17 09:07:14 +0900

    Add: AskView와 JobsView 적용 및 표현

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,24 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { fetchNewsList } from "../api/index.js";
+import { fetchJobsList, fetchNewsList, fetchAskList } from "../api/index.js";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
     news: [],
+    jobs: [],
+    ask: [],
   },
   mutations: {
     SET_NEWS(state, news) {
       state.news = news;
+    },
+    SET_JOBS(state, jobs) {
+      state.jobs = jobs;
+    },
+    SET_ASK(state, ask) {
+      state.ask = ask;
     },
   },
   actions: {
@@ -18,6 +26,20 @@ export const store = new Vuex.Store({
       fetchNewsList()
         .then((response) => {
           context.commit("SET_NEWS", response.data);
+        })
+        .catch((error) => console.log(error));
+    },
+    FETCH_JOBS(context) {
+      fetchJobsList()
+        .then((response) => {
+          context.commit("SET_JOBS", response.data);
+        })
+        .catch((error) => console.log(error));
+    },
+    FETCH_ASK(context) {
+      fetchAskList()
+        .then((response) => {
+          context.commit("SET_ASK", response.data);
         })
         .catch((error) => console.log(error));
     },

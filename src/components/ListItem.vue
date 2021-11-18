e3ea7c2bbbb27c5f297af24859ca1425389fa198
office1201 <office.div1201@gmail.com>
2021-11-18 17:25:32 +0900

    Add: 공통 컴포넌트 ListItem 생성 및 NewsView에 적용

---
+++ src/components/ListItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <ul class="news-list">
+      <li
+        v-for="item in this.$store.state.news"
+        v-bind:key="item.id"
+        class="post"
+      >
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 영역 -->
+        <div>
+          <p class="news-title">
+            <a v-bind:href="item.url">
+              {{ item.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by
+            <router-link v-bind:to="`/user/${item.user}`" class="link-text">
+              {{ item.user }}
+            </router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$store.dispatch("FETCH_NEWS");
+  },
+};
+</script>
+
+<style scoped>
+.news-list {
+  padding: 0;
+  margin: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #cacaca;
+}
+</style>
--- src/view/NewsView.vue
+++ src/view/NewsView.vue
@@ -1,92 +1,18 @@
 <template>
   <div>
-    <!-- #1 -->
-    <!-- <div v-for="user in this.$store.state.news" v-bind:key="user.id">
-      {{ user.title }}
-    </div> -->
-    <!-- #2 -->
-    <!-- <p v-for="item in this.$store.state.news" v-bind:key="item.id">
-      <a v-bind:href="item.url">
-        {{ item.title }}
-      </a>
-      <small>
-        {{ item.time_ago }} by
-        <router-link v-bind:to="`/user/${item.user}`">
-          {{ item.user }}
-        </router-link>
-      </small>
-    </p> -->
-    <!-- #3 -->
-    <ul class="news-list">
-      <li
-        v-for="item in this.$store.state.news"
-        v-bind:key="item.id"
-        class="post"
-      >
-        <!-- 포인트 영역 -->
-        <div class="points">
-          {{ item.points }}
-        </div>
-        <!-- 기타 영역 -->
-        <div>
-          <p class="news-title">
-            <a v-bind:href="item.url">
-              {{ item.title }}
-            </a>
-          </p>
-          <small class="link-text">
-            {{ item.time_ago }} by
-            <router-link v-bind:to="`/user/${item.user}`" class="link-text">
-              {{ item.user }}
-            </router-link>
-          </small>
-        </div>
-      </li>
-    </ul>
+    <ListItem></ListItem>
   </div>
 </template>
 
 <script>
-// import { fetchNewsList } from "../api/index.js";
+import ListItem from "../components/ListItem.vue";
 
 export default {
-  // data() {
-  //   return {
-  //     users: [],
-  //   };
-  // },
-  created() {
-    this.$store.dispatch("FETCH_NEWS");
-    // fetchNewsList()
-    //   .then((response) => (this.users = response.data))
-    //   .catch((error) => console.log(error));
+  components: {
+    ListItem,
   },
 };
 </script>
 
-<style scoped>
-.news-list {
-  padding: 0;
-  margin: 0;
-}
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-.points {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883;
-}
-.news-title {
-  margin: 0;
-}
-.link-text {
-  color: #cacaca;
-}
+<style>
 </style>

bca9ab76f54b0eda794a8d28abde75ceaf9784d3
office1201 <office.div1201@gmail.com>
2021-11-18 18:51:27 +0900

    Modify: 불필요한 태그 및 주석 제거

--- src/view/AskView.vue
+++ src/view/AskView.vue
@@ -1,58 +1,15 @@
 <template>
   <div>
     <ListItem></ListItem>
-    <!-- <ul class="news-list">
-      <li v-for="item in fetchedAsk" v-bind:key="item.id" class="post">
-        <div class="points">
-          {{ item.points }}
-        </div>
-        <div>
-          <p class="news-title">
-            <router-link v-bind:to="`item/${item.id}`">
-              {{ item.title }}
-            </router-link>
-          </p>
-          <small class="link-text">
-            {{ item.time_ago }} by
-            <router-link v-bind:to="`/user/${item.id}`" class="link-text">
-              {{ item.title }}
-            </router-link>
-          </small>
-        </div>
-      </li>
-    </ul> -->
   </div>
 </template>
 
 <script>
-// import { fetchAskList } from "../api/index.js";
-// import { mapState } from "vuex";
-// import { mapGetters } from "vuex";
 import ListItem from "../components/ListItem.vue";
 
 export default {
   components: {
     ListItem,
   },
-  //   // data() {
-  //   //   return {
-  //   //     ask: [],
-  //   //   };
-  //   // },
-  //   computed: mapGetters(["fetchedAsk"]),
-  //   // #1
-  //   // computed:
-  //   // mapState({
-  //   // ask: (state) => state.ask,
-  //   // }),
-  //   created() {
-  //     this.$store.dispatch("FETCH_ASK");
-  //     // fetchAskList()
-  //     //   .then((response) => (this.ask = response.data))
-  //     //   .catch((error) => console.log(error));
-  //   },
 };
 </script>
-
-<style>
-</style>
--- src/view/JobsView.vue
+++ src/view/JobsView.vue
@@ -1,28 +1,6 @@
 <template>
   <div>
     <ListItem></ListItem>
-    <!-- <ul class="news-list">
-      <li
-        v-for="job in this.$store.state.jobs"
-        v-bind:key="job.id"
-        class="post"
-      >
-        <div class="points">
-          {{ job.points || 0 }}
-        </div>
-        <div>
-          <p class="news-title">
-            <a v-bind:href="job.url">{{ job.title }}</a>
-          </p>
-          <small class="link-text">
-            {{ job.time_ago }} by
-            <a v-bind:href="job.url">
-              {{ job.domain }}
-            </a>
-          </small>
-        </div>
-      </li>
-    </ul> -->
   </div>
 </template>
 
@@ -35,6 +13,3 @@ export default {
   },
 };
 </script>
-
-<style>
-</style>
